df2a07a3d2785e06b337bd7fa841de63bb876821
Fatih Nejat <fatih.nejatyilmaz@gmail.com>
2022-10-22 15:50:41 +0300

    Header component created,Button generated

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Header from './components/header/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+function App() {
+  return (
+    <div className='contain text-center'>
+      <Header/>
+    
+
+
+      
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Header.css"
+
+const Header = () => {
+  return (
+    <div className='header'>
+     <h1>Task Tracker</h1>
+     <button className='btn btn-danger'>Show Add Task Bar</button>
+     
+     </div>
+    
+  
+  )
+}
+
+export default Header
